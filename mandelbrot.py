6a8be34816a7dd06dbc30bde5fd1487e4513a8f9
j3yxz <j3yxz@bitbucket.org>
2022-05-18 17:40:02 +0200

    now cv will generate a gray-scale fractal correctly but the code is really ugle and to refactor in a major way

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,12 +1,20 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sys import argv
+import cv2 as cv
+#######this is from opencv c++ code
 
-def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
-    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
-    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
-    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
 
+
+
+def complex_matrix(xmin, xmax, ymin, ymax, width, height):
+	#scaleX = (width/int(xmax-xmin))
+	#scaleY = (width/int(ymax-ymin))
+
+    re = np.linspace(xmin, xmax, width)
+    im = np.linspace(ymin, ymax, height)
+    #return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+    return re, im
 def is_stable(c, num_iterations):
     z = 0
     for _ in range(num_iterations):
@@ -19,7 +27,8 @@ def get_members(c, num_iterations):
 
 print("argv: ", argv[1])
 
-c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=300)
+c = complex_matrix(-2, 0.5, -1.5, 1.5, 4000, 4000)
+#print(c)
 #members = get_members(c, num_iterations=int(argv[1]))
 
 #non lo voglio stampare, ma nel caso:
@@ -54,21 +63,58 @@ class MandelbrotSet:
             if abs(z) > 2:
                 return iteration
         return self.max_iterations
+    
+    def sqrt_stability(self, c: complex) -> int:
+    	value = float(self.escape_count(c))
+    	if(int(value)-self.max_iterations == 0):
+    		return 0
+    	elif(int(value)==0):
+    		return 255
+    	else:
+    		return int( ((1 - value/self.max_iterations)**0.5)*255 +0.5)
+
+    def palette_x_channel(self, c: complex):
+    	integer_to_palette = self.sqrt_stability(c)
+    	R, G , B = integer_to_palette
+    	return R,G,B
+def create_set(c,max_iter,width,height):
+	#il magic number dovrà diventare il channel dei colori in qualche modo
+	#ret = np.empty_like([[0],[1]], dtype='float64')
+	ret = np.zeros((3, 3, 3), dtype = "uint8")
+	ret.resize(width,height,3)
+	#da impostare a max_iter
+	mandelbrotSet = MandelbrotSet(max_iterations=int(max_iter))
+	re, im = c
+	#print(re.shape)
+	for i in range(re.shape[0]):
+		for j in range(im.shape[0]):
+	#		print(re[i], im[j])
+			ret[j][i][0] = mandelbrotSet.sqrt_stability(complex(re[i]+im[j]*1j))
+			ret[j][i][1] = ret[j][i][0]
+			ret[j][i][2] = ret[j][i][0]
+	return ret
+
+
+max_iter = argv[1]
+width, height = 4000, 4000
+#np.array([width,height,1])
+
+
 
 
 ###############################
-mandelbrot_set = MandelbrotSet(max_iterations=20)
-width, height = 512, 512
+mandelbrot_set = MandelbrotSet(max_iterations=70)
+'''
 scale = 0.0075
 GRAYSCALE = "L"
 members = []
-print(c.size)
-
-a = np.array([[1, 2, 3],[4,5,6]])
+print(c.size, c.shape[0], c.shape[1], c)
+print(c[0][0], c[3][3])
+a = np.array([width, height, 3])
 image = np.empty_like(a)
 
-for i in range(c.shape[0]):
-    for j in range(c.shape[1]):
+for i in range(c.real):
+    for j in range(c.imag):
         p = mandelbrot_set.stability(c[i][j])
         pc = int(p * 255 / 3)
         np.append(image,pc)
@@ -76,18 +122,39 @@ for i in range(c.shape[0]):
         np.append(image,pc)
 
 #for index, pixel in enumerate(c):
-
+#to remove
+'''
+print("starting to write..")
     
+image_row = create_set(c, max_iter, width, height)
 
-import cv2 as cv
-cv.read_image(image, GRAYSCALE)
-cv.imwrite("mandelbrot.jpg", image)
+#perché?
+#cv.imread(image, GRAYSCALE)
+def zoom_on_coord(xc, yc, zoom_factor, img):
+#calc the images parameters
+	width = img.shape[1]
+	height = img.shape[0]
+#calc new borders
+	x1 = int(xc-0.5*width*(1-1/zoom_factor))
+	x2 = int(xc+0.5*width*(1-1/zoom_factor))
+	y1 = int(yc-0.5*height*(1-1/zoom_factor))
+	y2 = int(yc+0.5*height*(1-1/zoom_factor))
+	img_cropped = img[y1:y2,x1:x2]
+	print(img)
+	return 1#cv.resize(img, None, fx=zoom_factor, fy=zoom_factor)
+
+image = image_row # cv.imread(image_row, cv.IMREAD_GRAYSCALE)
+#image_z = zoom_on_coord(100, 100, 1.3, image)
+cv.imwrite("mandelbrot.png", image)
+#cv.imwrite("z.png", image_z)
 #for x in c.split(" "):
 #    print(x)
     #members.append(mandelbrot_set.stability(x))
 
 #membersGreyScale = mandelbrot_set.stability(c)
-print(members)
+#print(members)
+########################
+
 '''
 from viewport import Viewport
 from PIL import Image
